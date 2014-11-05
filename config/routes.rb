b42df84f7a59f86b1b2cea68ba1cc590a78a2573
Raghu Varma <raghuvarma@masymbol.com>
2014-11-05 10:06:11 +0530

    add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Posts::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :posts
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
