5c916d03a7aebafd3ce78134029a34ba554350a4
Raghu Varma <raghuvarma@masymbol.com>
2014-11-06 09:02:56 +0530

    add posts show action for displaying created post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,15 @@ class PostsController < ApplicationController
 		redirect_to @post
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	private
 	def post_params
 		params[:post]
 	end
-	
+
 	#def post_params
 		#params.require(:post).permit(:title, :content)
 	#end
